769adcb6008547f94d6338e558dfc8e915ca4c42
ysosrs123 <ysosrs123@users.noreply.github.com>
2026-07-26 07:30:40 +1000

    chore: version 0.7.20-beta-nt1 (1137)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -122,8 +122,8 @@ android {
         applicationId = "com.nuvio.tv.test"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1136
-        versionName = "0.7.19-beta-nt28"
+        versionCode = 1137
+        versionName = "0.7.20-beta-nt1"
 
         buildConfigField("String", "PARENTAL_GUIDE_API_URL", "\"${localProperties.getProperty("PARENTAL_GUIDE_API_URL", "")}\"")
         buildConfigField("String", "INTRODB_API_URL", "\"${localProperties.getProperty("INTRODB_API_URL", "")}\"")
