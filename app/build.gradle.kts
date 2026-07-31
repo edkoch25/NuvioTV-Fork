4101c46a4e736d337fb366247a454569a916ef30
ysosrs123 <ysosrs123@users.noreply.github.com>
2026-07-31 12:39:03 +1000

    build: bump to 0.7.20-beta-nt39 (1175)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -122,8 +122,8 @@ android {
         applicationId = "com.nuvio.tv.test"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1174
-        versionName = "0.7.20-beta-nt38"
+        versionCode = 1175
+        versionName = "0.7.20-beta-nt39"
 
         buildConfigField("String", "PARENTAL_GUIDE_API_URL", "\"${localProperties.getProperty("PARENTAL_GUIDE_API_URL", "")}\"")
         buildConfigField("String", "INTRODB_API_URL", "\"${localProperties.getProperty("INTRODB_API_URL", "")}\"")
