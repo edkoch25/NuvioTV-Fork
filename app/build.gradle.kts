a6ead60cd8b8483c5b84958f6bf96699391f3a65
ysosrs123 <ysosrs123@users.noreply.github.com>
2026-08-10 19:03:45 +1000

    chore(release): bump to 0.8.2-beta-nt46 / versionCode 1255

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,8 +100,8 @@ android {
         applicationId = "com.nuvio.tv.test"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1254
-        versionName = "0.8.2-beta-nt45"
+        versionCode = 1255
+        versionName = "0.8.2-beta-nt46"
 
         buildConfigField("String", "PARENTAL_GUIDE_API_URL", "\"${localProperties.getProperty("PARENTAL_GUIDE_API_URL", "")}\"")
         buildConfigField("String", "INTRODB_API_URL", "\"${localProperties.getProperty("INTRODB_API_URL", "")}\"")
