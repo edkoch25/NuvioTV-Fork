a4fad601c6cf36551a50774588d6c95fb3aa1f31
ysosrs123 <ysosrs123@users.noreply.github.com>
2026-08-15 21:35:12 +1000

    chore(release): version 0.8.4-beta-nt4 (versionCode 1290)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,8 +100,8 @@ android {
         applicationId = "com.nuvio.tv.test"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1289
-        versionName = "0.8.4-beta-nt3"
+        versionCode = 1290
+        versionName = "0.8.4-beta-nt4"
 
         buildConfigField("String", "PARENTAL_GUIDE_API_URL", "\"${localProperties.getProperty("PARENTAL_GUIDE_API_URL", "")}\"")
         buildConfigField("String", "INTRODB_API_URL", "\"${localProperties.getProperty("INTRODB_API_URL", "")}\"")
