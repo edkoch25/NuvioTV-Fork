330d4bcc0f88868235d39c9316f05e5e6ab331c7
ysosrs123 <ysosrs123@users.noreply.github.com>
2026-08-18 15:25:17 +1000

    fix: add missing Dp import for skip-button anchor callback (PlayerScreen)

--- app/src/main/java/com/nuvio/tv/ui/screens/player/PlayerScreen.kt
+++ app/src/main/java/com/nuvio/tv/ui/screens/player/PlayerScreen.kt
@@ -99,6 +99,7 @@ import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
