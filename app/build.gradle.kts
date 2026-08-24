e22ce5b590d164f752fde61edc841f03afe2a0a4
ysosrs123 <ysosrs123@users.noreply.github.com>
2026-08-24 11:34:59 +1000

    release: 0.8.5-beta-nt16 (versionCode 1328)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,8 +94,8 @@ android {
         applicationId = "com.nuvio.tv.test"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1327
-        versionName = "0.8.5-beta-nt15"
+        versionCode = 1328
+        versionName = "0.8.5-beta-nt16"
 
         buildConfigField("String", "PARENTAL_GUIDE_API_URL", "\"${localProperties.getProperty("PARENTAL_GUIDE_API_URL", "")}\"")
         buildConfigField("String", "INTRODB_API_URL", "\"${localProperties.getProperty("INTRODB_API_URL", "")}\"")
